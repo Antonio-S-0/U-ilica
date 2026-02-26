7e1b5bb7eda636ee1c357056ce56b048e04fee25
Antonio0234 <antoniosvoboda5@gmail.com>
2026-02-26 19:36:54 +0100

    Faza 3

--- Projektni zadatak  1/src/GeometrijskiLik.java
+++ Projektni zadatak  1/src/GeometrijskiLik.java
@@ -1,9 +1,6 @@
-public abstract class GeometrijskiLik {
+public abstract class GeometrijskiLik implements Comparable<GeometrijskiLik> {
 
     private String naziv;
-
-    public abstract double opseg();
-    public abstract double povrsina();
 
     public GeometrijskiLik(String naziv) {
         this.naziv = naziv;
@@ -15,5 +12,20 @@ public abstract class GeometrijskiLik {
 
     public void setNaziv(String naziv) {
         this.naziv = naziv;
+    }
+
+    public abstract double opseg();
+    public abstract double povrsina();
+
+    @Override
+    public int compareTo(GeometrijskiLik other) {
+        return Double.compare(this.povrsina(), other.povrsina());
+    }
+
+    @Override
+    public String toString() {
+        return "Naziv: " + naziv +
+                ", Povrsina: " + povrsina() +
+                ", Opseg: " + opseg();
     }
 }
--- Projektni zadatak  1/src/Main.java
+++ Projektni zadatak  1/src/Main.java
@@ -1,19 +1,88 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+        int brojacKrug = 1;
+        int brojacTrokut = 1;
+        int brojacPravokutnik = 1;
+        Scanner sc = new Scanner(System.in);
+        ArrayList<GeometrijskiLik> generiraniLikovi = new ArrayList<>();
 
-        Krug k = new Krug(10, "Krug1");
-        System.out.println("Naziv: " + k.getNaziv());
-        System.out.println("Opseg: " + k.opseg());
-        System.out.println("Povrsina: " + k.povrsina());
+        int izbor;
 
-        Pravokutnik p = new Pravokutnik(4, 6, "Prav1");
-        System.out.println("\nNaziv: " + p.getNaziv());
-        System.out.println("Opseg: " + p.opseg());
-        System.out.println("Povrsina: " + p.povrsina());
+        do {
+            System.out.println("\n1 - Dodaj Trokut");
+            System.out.println("2 - Dodaj Krug");
+            System.out.println("3 - Dodaj Pravokutnik");
+            System.out.println("4 - Ispisi sve");
+            System.out.println("5 - Sortiraj po povrsini (uzlazno)");
+            System.out.println("0 - Izlaz");
 
-        Trokut t = new Trokut(3, 4, 5, "Trokut1");
-        System.out.println("\nNaziv: " + t.getNaziv());
-        System.out.println("Opseg: " + t.opseg());
-        System.out.println("Povrsina: " + t.povrsina());
+            System.out.print("Odabir: ");
+            izbor = sc.nextInt();
+            sc.nextLine();
+
+            switch (izbor) {
+
+                case 1:
+                    System.out.print("a: ");
+                    double a = sc.nextDouble();
+                    System.out.print("b: ");
+                    double b = sc.nextDouble();
+                    System.out.print("c: ");
+                    double c = sc.nextDouble();
+                    sc.nextLine();
+
+                    String nazivTrokuta = "Trokut" + brojacTrokut++;
+                    generiraniLikovi.add(new Trokut(a, b, c, nazivTrokuta));
+                    break;
+                case 2:
+                    System.out.print("Radijus: ");
+                    double r = sc.nextDouble();
+                    sc.nextLine();
+
+                    String nazivKruga = "Krug" + brojacKrug++;
+                    generiraniLikovi.add(new Krug(r, nazivKruga));
+                    break;
+
+                case 3:
+                    System.out.print("a: ");
+                    double pa = sc.nextDouble();
+                    System.out.print("b: ");
+                    double pb = sc.nextDouble();
+                    sc.nextLine();
+
+                    String nazivPrav = "Pravokutnik" + brojacPravokutnik++;
+                    generiraniLikovi.add(new Pravokutnik(pa, pb, nazivPrav));
+                    break;
+
+                case 4:
+                    for (GeometrijskiLik g : generiraniLikovi) {
+                        System.out.println(g);
+                    }
+                    break;
+
+                case 5:
+                    Collections.sort(generiraniLikovi);
+                    System.out.println("Sortirano!");
+                    for (GeometrijskiLik g : generiraniLikovi) {
+                        System.out.println(g);
+                    }
+                    break;
+
+                case 0:
+                    System.out.println("Izlaz.");
+                    break;
+
+                default:
+                    System.out.println("Neispravan unos.");
+            }
+
+        } while (izbor != 0);
+
+        sc.close();
     }
 }
